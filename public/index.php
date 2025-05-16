97924c7b7e14ea8b3168cd98783da3899dd158ea
diegomaustem <diego.maustem@gmail.com>
2025-05-16 10:45:01 -0300

    :recycle: refactor: Ajuste de error_log no index

--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@ header('Content-Type: application/json; charset=utf-8');
 try {
     Route::direcionaRota();
 } catch (Throwable $th) {
+    error_log("Erro interno:" . $th->getMessage());
     http_response_code(500);
     exit(json_encode(["error" =>"Erro interno: configuração de rotas indisponível"], JSON_UNESCAPED_UNICODE));
 }
